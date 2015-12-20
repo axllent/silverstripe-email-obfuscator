43fa52ad50258f907955352e3511e491978204af
helpfulrobot <chris@silverstripe.com>
2015-12-20 16:23:40 +1300

    Converted to PSR-2

--- code/EmailObfuscatorRequestProcessor.php
+++ code/EmailObfuscatorRequestProcessor.php
@@ -14,69 +14,73 @@
  * Authors: Techno Joy development team (www.technojoy.co.nz)
  */
 
-class EmailObfuscatorRequestProcessor implements RequestFilter {
+class EmailObfuscatorRequestProcessor implements RequestFilter
+{
 
-	/**
-	 * Filter executed AFTER a request
-	 * Run output through ObfuscateEmails filter
-	 * encoding emails in the $response
-	 */
-	public function postRequest(SS_HTTPRequest $request, SS_HTTPResponse $response, DataModel $model) {
-		if (preg_match('/text\/html/', $response->getHeader('Content-Type'))) {
-			$response->setBody(
-				$this->ObfuscateEmails($response->getBody())
-			);
-		}
-	}
+    /**
+     * Filter executed AFTER a request
+     * Run output through ObfuscateEmails filter
+     * encoding emails in the $response
+     */
+    public function postRequest(SS_HTTPRequest $request, SS_HTTPResponse $response, DataModel $model)
+    {
+        if (preg_match('/text\/html/', $response->getHeader('Content-Type'))) {
+            $response->setBody(
+                $this->ObfuscateEmails($response->getBody())
+            );
+        }
+    }
 
-	/*
-	 * Obfuscate all matching emails
-	 * @param string
-	 * @return string
-	 */
-	public function ObfuscateEmails($html) {
-		$reg = '/[:_a-z0-9-+]+(\.[_a-z0-9-+]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})/i';
-		if (preg_match_all($reg, $html, $matches)) {
-			$searchstring = $matches[0];
-			for ($i=0; $i < count($searchstring); $i++)
-				$html = preg_replace(
-					'/' . $searchstring[$i] . '/',
-					$this->encode($searchstring[$i]),
-					$html
-				);
-		}
-		return $html;
-	}
+    /*
+     * Obfuscate all matching emails
+     * @param string
+     * @return string
+     */
+    public function ObfuscateEmails($html)
+    {
+        $reg = '/[:_a-z0-9-+]+(\.[_a-z0-9-+]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})/i';
+        if (preg_match_all($reg, $html, $matches)) {
+            $searchstring = $matches[0];
+            for ($i=0; $i < count($searchstring); $i++) {
+                $html = preg_replace(
+                    '/' . $searchstring[$i] . '/',
+                    $this->encode($searchstring[$i]),
+                    $html
+                );
+            }
+        }
+        return $html;
+    }
 
-	/**
-	 * Obscure email address.
-	 *
-	 * @param string The email address
-	 * @return string The encoded (ASCII & hexadecimal) email address
-	 */
-	protected function encode($originalString) {
-		$encodedString = '';
-		$nowCodeString = '';
-		$originalLength = strlen($originalString);
-		for ($i = 0; $i < $originalLength; $i++) {
-			$encodeMode = ($i % 2 == 0) ? 1 : 2; // Switch encoding odd/even
-			switch ($encodeMode) {
-				case 1: // Decimal code
-					$nowCodeString = '&#' . ord($originalString[$i]) . ';';
-					break;
-				case 2: // Hexadecimal code
-					$nowCodeString = '&#x' . dechex(ord($originalString[$i])) . ';';
-					break;
-				default:
-					return 'ERROR: wrong encoding mode.';
-			}
-			$encodedString .= $nowCodeString;
-		}
-		return $encodedString;
-	}
+    /**
+     * Obscure email address.
+     *
+     * @param string The email address
+     * @return string The encoded (ASCII & hexadecimal) email address
+     */
+    protected function encode($originalString)
+    {
+        $encodedString = '';
+        $nowCodeString = '';
+        $originalLength = strlen($originalString);
+        for ($i = 0; $i < $originalLength; $i++) {
+            $encodeMode = ($i % 2 == 0) ? 1 : 2; // Switch encoding odd/even
+            switch ($encodeMode) {
+                case 1: // Decimal code
+                    $nowCodeString = '&#' . ord($originalString[$i]) . ';';
+                    break;
+                case 2: // Hexadecimal code
+                    $nowCodeString = '&#x' . dechex(ord($originalString[$i])) . ';';
+                    break;
+                default:
+                    return 'ERROR: wrong encoding mode.';
+            }
+            $encodedString .= $nowCodeString;
+        }
+        return $encodedString;
+    }
 
-	public function preRequest(SS_HTTPRequest $request, Session $session, DataModel $model) {
-
-	}
-
+    public function preRequest(SS_HTTPRequest $request, Session $session, DataModel $model)
+    {
+    }
 }
